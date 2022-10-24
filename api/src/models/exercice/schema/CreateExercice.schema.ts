799bd5c7634ab74a7c25d738577eb626610bb915
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-10-24 14:25:08 +0200

    added exercice swagger schema

---
+++ api/src/models/exercice/schema/CreateExercice.schema.ts
@@ -0,0 +1,90 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { ObjectId } from 'mongodb';
+import { BodyPartEnum } from '../interface/bodyPart.enum';
+import { WeightUnitEnum } from '../interface/weightUnit.enum';
+
+/** DESCRIPTION */
+
+class CreateExerciceDescriptionSchema {
+  @ApiProperty({
+    type: String,
+    description: 'description',
+    example: 'ceci est ma description',
+  })
+  description: string;
+
+  @ApiProperty({ type: Number, description: 'raiting', example: '5' })
+  raiting: number;
+
+  @ApiProperty({
+    enum: BodyPartEnum,
+    description: 'bodyPart',
+    example: [BodyPartEnum.BRAS],
+    required: true,
+    isArray: true,
+  })
+  bodyPart: BodyPartEnum[];
+
+  @ApiProperty({
+    type: String,
+    description: 'urlImage',
+    example: 'face-pull-exercice-epaules.jpg',
+  })
+  imageUrl: string;
+}
+
+/** REALISATION */
+class CreateExerciceRealisationSchema {
+  @ApiProperty({
+    type: Number,
+    description: 'recoveryTime',
+    example: [11, 22, 55],
+    isArray: true,
+  })
+  recoveryTime: number[];
+
+  @ApiProperty({
+    type: Number,
+    description: 'series',
+    example: 3,
+  })
+  series: number;
+
+  @ApiProperty({
+    type: Number,
+    description: 'repetition',
+    example: [11, 12, 13],
+    isArray: true,
+  })
+  repetition: number[];
+
+  @ApiProperty({
+    type: String,
+    description: 'weight',
+    isArray: true,
+  })
+  weight: { number?: number; unit: WeightUnitEnum }[];
+}
+
+/**EXERCICE */
+export class CreateExerciceSchema {
+  @ApiProperty({ type: String, description: 'title', example: 'curl' })
+  title: string;
+
+  @ApiProperty({
+    type: String,
+    description: 'creationUser',
+    example: '111144487',
+  })
+  creationUser?: ObjectId | null;
+
+  @ApiProperty({
+    type: CreateExerciceDescriptionSchema,
+  })
+  description: CreateExerciceDescriptionSchema;
+
+  @ApiProperty({
+    type: CreateExerciceRealisationSchema,
+  })
+  realisation: CreateExerciceRealisationSchema;
+}
