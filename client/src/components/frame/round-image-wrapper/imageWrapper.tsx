a727025235638c51eb31377b1ddc374511560d1f
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-10-30 17:33:03 +0100

    improvement image wrapper circle

--- client/src/components/frame/round-image-wrapper/imageWrapper.tsx
+++ client/src/components/frame/round-image-wrapper/imageWrapper.tsx
@@ -3,7 +3,7 @@ import "./imageWrapper.scss";
 const ImageWrapper = ({ imgUrl, alt }: { imgUrl: string; alt: string }) => {
   return (
     <div className="img-wrapper">
-      <img src={"../assets/img/exercicePost/" + imgUrl} alt={alt} />
+      <img src={"../assets/img/" + imgUrl} alt={alt} />
     </div>
   );
 };
