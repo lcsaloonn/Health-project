d3cff61167f81d9e6113e1c8f915cc8fee3d4d78
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-11-03 10:56:25 +0100

    added argument

--- client/src/components/forms/elements/inputWeight/inputWeight.component.tsx
+++ client/src/components/forms/elements/inputWeight/inputWeight.component.tsx
@@ -1,20 +1,18 @@
-import { useState } from "react";
 import { WeightUnitEnum } from "utils/types/exercice/enum/weightUnit.enum";
 import { InputNUmberComponent } from "../inputNumber/inputNumber.component";
 import "./inputWeight.scss";
 
-export function InputWeight() {
-  const [weight, setWeigh] = useState(0);
+export function InputWeight({
+  maxValue,
+  unit,
+}: {
+  maxValue: number;
+  unit: WeightUnitEnum;
+}) {
   return (
     <div className="input-weight">
-      <InputNUmberComponent />
-      <select>
-        {Object.values(WeightUnitEnum).map((element: string, id: number) => (
-          <option value={element} key={id}>
-            {element}
-          </option>
-        ))}
-      </select>
+      <InputNUmberComponent maxvalue={maxValue} />
+      <span>{unit}</span>
     </div>
   );
 }
